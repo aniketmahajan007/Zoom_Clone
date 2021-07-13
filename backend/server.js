1af13297a6f9194b8e7df8e87b4eb03b79a3fe77
Aniket Mahajan <007aniketmahajan@gmail.com>
2021-07-13 22:27:39 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,41 @@
-const {ExpressPeerServer} = require("frontend/js/peer");
+const {ExpressPeerServer} = require("peer");
 const app = require('express')();
 const server = require('http').Server(app);
 const io = require('socket.io')(server,{
     cors: 'http://localhost:5000'
 });
-const {v4: uuid} = require('uuid');
+const {v1: uuid} = require('uuid');
+const cors = require('cors');
 const peerServer = ExpressPeerServer(server,{
     debug: true
 })
-
+app.use(cors());
 app.use('/peerjs',peerServer);
 
 io.on('connection',socket=>{
-    socket.on('join-room',(Roomid, Userid)=>{
-        socket.join(Roomid);
-        socket.to(Roomid).emit('user-connect', Userid);
-    })
+    socket.on('create-room',new_roomid => {
+        socket.join(new_roomid);
+    });
+    socket.on('join-room', Roomid=>{
+        let flag=false;
+        socket.adapter.rooms.forEach((index,value)=>{
+           if(value === Roomid){
+               flag=true;
+               console.log(value, value.length);
+               console.log('hello');
+               socket.join(Roomid);
+               console.log(value, value.length);
+           }
+        });
+        console.log(flag);
+        socket.emit('error','invalid_id')
+        /*if(flag){
+            socket.join(Roomid);
+            socket.emit('join-allowed','Access Granted');
+        }else{
+            socket.emit('error','invalid_id')
+        }*/
+    });
 });
 
 server.listen(3000);

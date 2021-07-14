6073abdc428034cd097fa02e3255e2364f2c0697
Aniket Mahajan <007aniketmahajan@gmail.com>
2021-07-14 22:05:55 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -16,25 +16,25 @@ io.on('connection',socket=>{
     socket.on('create-room',new_roomid => {
         socket.join(new_roomid);
     });
+    socket.on('disconnect',()=>{
+        socket.broadcast.emit('user-leave-broadcast',socket.id);
+    })
     socket.on('join-room', Roomid=>{
-        let flag=false;
-        socket.adapter.rooms.forEach((index,value)=>{
-           if(value === Roomid){
-               flag=true;
-               console.log(value, value.length);
-               console.log('hello');
-               socket.join(Roomid);
-               console.log(value, value.length);
-           }
-        });
-        console.log(flag);
-        socket.emit('error','invalid_id')
-        /*if(flag){
-            socket.join(Roomid);
-            socket.emit('join-allowed','Access Granted');
+        if(socket.adapter.rooms.get(Roomid) !== undefined){
+            if(socket.adapter.rooms.get(Roomid).size === 1){
+                let room_creater;
+                socket.adapter.rooms.get(Roomid).forEach((value)=>{
+                    room_creater = value;
+                });
+                socket.join(Roomid);
+                socket.emit('join-allowed',room_creater);
+                socket.to(room_creater).emit('join-user-id',socket.id);
+            }else{
+                socket.emit('error','invalid_id');
+            }
         }else{
-            socket.emit('error','invalid_id')
-        }*/
+            socket.emit('error','invalid_id');
+        }
     });
 });
 
